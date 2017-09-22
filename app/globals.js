0c2e18c4cfd8534aab2523d57dbf421202dd2605
M5M400 <support@supportxmr.com>
2017-09-22 17:53:14 +0200

    Update globals.js

--- app/globals.js
+++ app/globals.js
@@ -6,7 +6,7 @@ angular.module('pool.globals', [])
 	return {
 		pool_name: "supportXMR.com",
 		api_url : 'https://supportxmr.com/api',
-		api_refresh_interval: 6000,
+		api_refresh_interval: 15000,
 		app_update_interval: 10*60000
 	};
 });
